48607d3ae0017f8fd281107ea622d881de3f0bd5
Al Carruth <al.carruth@gmail.com>
2020-05-01 12:56:45 -0500

    removed als-austin-map refs from bin/defs.bash

--- bin/defs.bash
+++ bin/defs.bash
@@ -1,47 +1,50 @@
 #!/bin/bash
 
 function clean() {
-    rm -r dist/ build/
+    echo "clean()"
+    rm -rf dist/ build/
     mkdir -p dist/css dist/js dist/images
     mkdir -p build/css build/js build/coffee build/images
 }
 
 function mk_neighborhood_map_coffee() {
-    pushd src/coffee
+    echo "mk_neighborhood_map_coffee()"
+    pushd src/coffee >/dev/null
     cat \
         jsonp_controller.coffee \
         google_maps_api.coffee \
         wikipedia_api.coffee \
         neighborhood_map.coffee \
-        > ../../build/coffee/neighboor-map.coffee
-    popd
+        > ../../build/coffee/neighborhood-map.coffee
+    popd >/dev/null
 }
 
 function mk_neighborhood_map_js() {
-    mk_als_neighborhood_map_coffee
-    pushd build
+    mk_neighborhood_map_coffee
+    echo "mk_neighborhood_map_js()"
+    pushd build >/dev/null
     coffee -c -o js/ coffee/neighborhood-map.coffee
-    popd
+    popd >/dev/null
 }
 
 function mk_build() {
     mk_neighborhood_map_js
-    pushd build
-    cp -r ../src/js .
+    echo "mk_build()"
+    pushd build >/dev/null
     cp -r ../src/css .
     cp -r ../src/images .
     cp ../node_modules/knockout/build/output/knockout-latest.js js/knockout.js
-    popd
+    popd >/dev/null
 }
 
 function mk_dist() {
     clean
     mk_build
-    pushd build
+    echo "mk_dist()"
+    pushd build >/dev/null
     cp -r js/ ../dist/
     cp -r css/ ../dist/
     cp -r images/ ../dist/
-    cp html/index.html ../dist/
-    popd
+    popd >/dev/null
 }
 
